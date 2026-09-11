6ccbf61121506562a26d5e0443de070f682b949e
Saif Sufian Tonmoy <sstonmoy777@gmail.com>
2026-09-11 15:51:16 +0600

    Removed files for now

---
+++ Chapter00/WhileLoop.py
@@ -0,0 +1,6 @@
+i = 1
+while (i < 51):
+    print(i)
+    i = i + 1
+
+# this program gives output from 1 to 50
---
+++ Chapter00/WhileLoop2.py
@@ -0,0 +1,6 @@
+# Write a program to print the content of a list using while loops
+l = [1, 2.43, 'Hossain', True]
+i = 0
+while (i < 2):
+    print(l)
+    i += 1
